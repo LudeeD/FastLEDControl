d93117292d3b46f4de1e9eeafaeada15f6906cb0
LudeeD <lfssilva@ua.pt>
2022-10-07 16:43:14 +0100

    Revert raw access to matrix, not better
    This reverts commit b52619ba4323d5cebf9a2f7e63830df5119fd86d.

--- src/led_control.hpp
+++ src/led_control.hpp
@@ -7,26 +7,12 @@ namespace teton {
 
 // TODO: Implement this function. Make it as fast as possible.
 bool computeLEDSignalFromImageBrightness(const cv::Mat &image) {
-    CV_Assert(image.depth() == CV_8U);
 
-    static int channels = image.channels();
-    static int nRows = image.rows;
-    static int nCols = image.cols * channels;
-    static int nTotal = nRows * nCols;
 
-    int i,j;
-    const uchar* p;
-    double sum = 0;
-    for( i = 0; i < nRows; ++i)
-    {
-        p = image.ptr<uchar>(i);
-        for ( j = 0; j < nCols; ++j)
-        {
-            sum += p[j];
-        }
-    }
+    cv::Scalar tempVal = cv::mean( image );
+    float myMAtMean = tempVal.val[0];
 
-    return (sum / nTotal) < 40;
+    return myMAtMean < 40;
 }
 
 }  // namespace teton
